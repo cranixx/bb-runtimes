aa3e423b7f6988725d5f7d3937ec91ecd3779851
Cranix <cranix@hackerspace.pl>
2017-09-12 12:30:49 +0200

    Added example that lights up led on PA5. (LD2 on nucleof103rb)

---
+++ examples/stm32f103/led_on/src/led_on.adb
@@ -0,0 +1,25 @@
+with System;
+use System;
+with Interfaces.STM32; use Interfaces.STM32;
+with Interfaces.STM32.GPIO; use Interfaces.STM32.GPIO;
+with Interfaces.STM32.RCC; use Interfaces.STM32.RCC;
+
+procedure led_on
+is
+   RCC_APB2ENR: UInt32;
+
+	 GPIOA : GPIO_Peripheral;
+   for RCC_APB2ENR'Address    use System'To_Address (16#4002_1018#);
+   for GPIOA'Address use System'To_address(16#4001_0800#);
+
+begin
+   RCC_APB2ENR:= RCC_APB2ENR or 2#1100#;
+   GPIOA.CRL.CNF5 := 2#00#;
+   GPIOA.CRL.MODE5 := 2#11#;
+
+    GPIOA.ODR.ODR.Arr(5) := 1;
+	 loop
+		 null;
+	 end loop;
+end led_on;
+
