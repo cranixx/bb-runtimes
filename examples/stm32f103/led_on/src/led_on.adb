cad2aba0fd8189b27d2414741509d014e6993281
Cranix <cranix@hackerspace.pl>
2017-09-12 12:37:53 +0200

    Code was changed to use rcc bindings.

--- examples/stm32f103/led_on/src/led_on.adb
+++ examples/stm32f103/led_on/src/led_on.adb
@@ -6,14 +6,15 @@ with Interfaces.STM32.RCC; use Interfaces.STM32.RCC;
 
 procedure led_on
 is
-   RCC_APB2ENR: UInt32;
-
+--   RCC_APB2ENR: UInt32;
+   RCC_APB2ENR : APB2ENR_Register;
 	 GPIOA : GPIO_Peripheral;
    for RCC_APB2ENR'Address    use System'To_Address (16#4002_1018#);
    for GPIOA'Address use System'To_address(16#4001_0800#);
 
 begin
-   RCC_APB2ENR:= RCC_APB2ENR or 2#1100#;
+ --  RCC_APB2ENR:= RCC_APB2ENR or 2#1100#;
+   RCC_APB2ENR.IOPAEN := 1;
    GPIOA.CRL.CNF5 := 2#00#;
    GPIOA.CRL.MODE5 := 2#11#;
 
